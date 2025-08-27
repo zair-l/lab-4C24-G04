55721ba6ed3aa13e860229a9fe6280d56b9b9032
Zair Trivino <trivinoszair@gmail.com>
2025-08-26 23:47:58 -0500

    Responsable: modificación en StudentController

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,5 +1,6 @@
 public class StudentController {
     public void controller() {
         System.out.println("Método controller en ejecución...");
+        System.out.println("Cambio realizado por el Responsable.");
     }
 }
